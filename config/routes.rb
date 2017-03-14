2690c415cc21f7e2dfb413b5688d5bacca844e63
Max Glassie <msglassie@gmail.com>
2017-03-14 14:49:55 -0600

    fixes fabricator for payments, starting on index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :invoices, only: [:index]
-      resources :merchants, only: [:index, :show, :create, :update]
+      resources :merchants, except: [:new, :edit]
+      resources :payments, only
     end
   end
 end
