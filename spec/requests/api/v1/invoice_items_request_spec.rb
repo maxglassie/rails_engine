0c79cbee4a1f9bcd5421dac508d96baf908b92fe
Edilene <edilene_cruz@yahoo.com.br>
2017-03-14 14:09:50 -0600

    completes invoice_items_request test

---
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "InvoiceItems API" do
+  it "creates a list of all invoice items" do
+    Fabricate.times(10, :invoice_item)
+
+    get '/api/v1/invoice_items'
+
+    expect(response).to be_success
+    invoice_items = JSON.parse(response.body)
+
+    expect(invoice_items.count).to eq(10)
+  end
+
+  it "can get one invoice item by its id" do
+    id = Fabricate(:invoice_item).id
+
+    get "/api/v1/invoice_items/#{id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(id)
+  end
+end
