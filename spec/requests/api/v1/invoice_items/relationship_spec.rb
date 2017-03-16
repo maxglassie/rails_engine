c40f18369a81e74520ca416dcd4dfdf17015495a
Edilene <edilene_cruz@yahoo.com.br>
2017-03-16 11:53:37 -0600

    adds invoice items relationship

--- spec/requests/api/v1/invoice_items/relationship_spec.rb
+++ spec/requests/api/v1/invoice_items/relationship_spec.rb
@@ -1,1 +1,30 @@
 require 'rails_helper'
+
+describe "InvoiceItems Relationship Endpoint" do
+  it "returns the associated invoice" do
+    invoice = Fabricate(:invoice)
+    invoice_item = Fabricate(:invoice_item, invoice: invoice)
+
+      get "/api/v1/invoice_items/#{invoice_item.id}/invoice"
+
+      expect(response).to be_success
+
+      invoice_item_invoice = JSON.parse(response.body)
+
+      expect(invoice_item.invoice["id"]).to eq(invoice.id)
+  end
+
+  it "returns the associated item for the invoice item" do
+    item = Fabricate(:item)
+    invoice_item = Fabricate(:invoice_item, item: item)
+
+    get "/api/v1/invoice_items/#{invoice_item.id}/item"
+
+    expect(response).to be_success
+
+    invoice_item_item = JSON.parse(response.body)
+
+    expect(item.id).to eq(invoice_item.item["id"])
+    expect(item.name).to eq(invoice_item.item["name"])
+  end
+end
