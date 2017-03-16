7ddafaeaeda3d8b6b7ce4141c06b22951d7b4766
Max Glassie <msglassie@gmail.com>
2017-03-15 18:26:39 -0600

    adds the payments to citext migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316000525) do
+ActiveRecord::Schema.define(version: 20170316001741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20170316000525) do
     t.integer  "invoice_id"
     t.string   "credit_card_number"
     t.datetime "credit_card_expiration_date"
-    t.string   "result"
+    t.citext   "result"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
