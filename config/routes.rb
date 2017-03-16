0b1b6300a6655fd63977e9daecb7151a60ea8786
Edilene <edilene_cruz@yahoo.com.br>
2017-03-16 11:30:45 -0600

    adds relationship for items

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,8 @@ Rails.application.routes.draw do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+        get '/:id/invoice_items', to: 'invoice_items#index'
+        get '/:id/merchant', to: 'merchant#show'
       end
       resources :items, only: [:index, :show]
 
